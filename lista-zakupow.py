a6cb5b3314b78ddf7f263d26171d4b8907a5afcf
pogmac <pogorzelski.maciej@gmail.com>
2022-12-29 20:12:32 +0100

    Modify lista-zakupow.py add code

--- lista-zakupow.py
+++ lista-zakupow.py
@@ -6,4 +6,15 @@ lista_zakupow = {
     "warzywniak": ["marchew","seler","rukola"]
 }
 
+sump = 0
+
+for k, v in lista_zakupow.items():
+   print(f"Idę do {k.capitalize()}, kupuję tu następujące rzeczy: ", end ="")
+   sump += len(v)
+   for i in range (len(v)):
+        if i != len(v)-1:
+            print(f" {lista_zakupow[k][i].capitalize()}" , end = ",") 
+        else:
+            print(f" {lista_zakupow[k][i].capitalize()}")     
+print(f"W sumie kupuję {sump} produktów")
 
