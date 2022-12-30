e691fab25e647f28540b437a49e1d064d843348c
pogmac <pogorzelski.maciej@gmail.com>
2022-12-30 12:50:34 +0100

    Pozdrowienia dla Patryka

--- lista-zakupow.py
+++ lista-zakupow.py
@@ -1,24 +1,2 @@
-print()
-print("Lista zakupów")
-print()
-
-lista_zakupow = {
-    "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew","seler","rukola"]
-}
-
-sump = 0
-for k, v in lista_zakupow.items():
-   print(f"Idę do {k.capitalize()}, kupuję tu następujące rzeczy: ", end ="")
-   sump += len(v)
-   for i in range (len(v)):
-        if i != len(v)-1:
-            print(f" {lista_zakupow[k][i].capitalize()}" , end = ",") 
-        else:
-            print(f" {lista_zakupow[k][i].capitalize()}")   
-
-print();print(f"W sumie kupuję {sump} produktów");print()
-
-print("To jest tekst zmian na 2 commit")
-print("To jest tekst zmian na 3 commit")
+print("Pozdrowienia dla Patryka")
 
