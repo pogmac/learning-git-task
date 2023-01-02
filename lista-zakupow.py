b665076684bbb8795a7802fd6f8c31c7870b2283
pogmac <pogorzelski.maciej@gmail.com>
2023-01-02 17:53:54 +0100

    Modify lista-zakupow.py

--- lista-zakupow.py
+++ lista-zakupow.py
@@ -1,2 +1,20 @@
-print("Pozdrowienia dla Patryka")
+print("lista zakupów")
 
+
+lista_zakupow = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew","seler","rukola"]
+}
+
+#print(lista_zakupow.keys())
+sump = 0
+
+for k, v in lista_zakupow.items():
+   print(f"Idę do {k.capitalize()}, kupuję tu następujące rzeczy: ", end ="")
+   sump += len(v)
+   for i in range (len(v)):
+        if i != len(v)-1:
+            print(f" {lista_zakupow[k][i].capitalize()}" , end = ",") 
+        else:
+            print(f" {lista_zakupow[k][i].capitalize()}")     
+print(f"W sumie kupuję {sump} produktów")
